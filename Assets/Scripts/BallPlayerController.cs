d92f45523e6de0a2aa442bd8b48ef833c150c110
QuincyJacobs <o0omister_qo0o@hotmail.com>
2019-10-22 22:13:54 +0200

    Initial project

---
+++ Assets/Scripts/BallPlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallPlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rigidBody;
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        var moveHorizontal = Input.GetAxis("Horizontal");
+        var moveVertical = Input.GetAxis("Vertical");
+
+        Debug.Log(rigidBody.velocity);
+
+        rigidBody.AddForce(new Vector3(-moveVertical, 0.0f, moveHorizontal) * speed);
+        if(rigidBody.velocity.x > 10)
+        {
+            rigidBody.velocity = new Vector3(10, rigidBody.velocity.y, rigidBody.velocity.z);
+        }
+        else if(rigidBody.velocity.x < -10)
+        {
+            rigidBody.velocity = new Vector3(-10, rigidBody.velocity.y, rigidBody.velocity.z);
+        }
+        if (rigidBody.velocity.z > 10)
+        {
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y, 10);
+        }
+        else if (rigidBody.velocity.z < -10)
+        {
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y, -10);
+        }
+        Debug.Log(rigidBody.velocity);
+    }
+}
---
+++ Assets/Scripts/FollowPlayerCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerCamera : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 _offset;
+
+    void Start()
+    {
+        _offset = new Vector3(1.25f, 2.8f, 0);
+    }
+
+    void Update()
+    {
+        transform.position = player.transform.position + _offset;
+        transform.LookAt(player.transform);
+    }
+}
